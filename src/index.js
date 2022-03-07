1634fb4aca6f99d1d2f2c6f9b39eaab6c0b6d76b
isma1996 <51497330+isma1996@users.noreply.github.com>
2022-03-07 15:51:52 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -17,7 +16,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
